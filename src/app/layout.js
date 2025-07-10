e39eb47145bc1e987969a6fbd889a3e99f1bf917
Muskan <muskanchhabra.0806@gmail.com>
2025-07-10 17:19:17 +0530

    deploying

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
 
+// {Suspense}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Suspense>
+          {children}
+        </Suspense>
+        
       </body>
     </html>
   );
